5f64424adcafab613e746bdbafcb4c919d1b7ad5
Demi226 <odd226@usask.ca>
2023-09-17 13:59:27 -0600

    Player Animation

--- Lavagame/Assets/Scenes/Working Scenes/Andrew_Working/Scripts/PlayerMovement.cs
+++ Lavagame/Assets/Scenes/Working Scenes/Andrew_Working/Scripts/PlayerMovement.cs
@@ -30,8 +30,12 @@ public class PlayerMovement : MonoBehaviour
     // how fast the player can move in any given direction
     private Vector2 speedX, speedY;
 
+    private Animator animator;
+
+
     void Update()
     {
+        animator = GetComponent<Animator>();
         // updates speed
         speedX = new Vector2(playerSpeed + boostTime, 0); 
         speedY = new Vector2(0, playerSpeed + boostTime);
@@ -40,18 +44,43 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey("a"))
         {
             chacacterController.GetComponent<Rigidbody2D>().MovePosition(chacacterController.GetComponent<Rigidbody2D>().position - speedX);
+            animator.SetBool("isMovingRight", false);
+            animator.SetBool("isMovingDown", false);
+            animator.SetBool("isMovingLeft", true);
+            animator.SetBool("isMovingUp", false);
         }
         else if (Input.GetKey("d"))
         {
             chacacterController.GetComponent<Rigidbody2D>().MovePosition(chacacterController.GetComponent<Rigidbody2D>().position + speedX);
+            animator.SetBool("isMovingRight", true);
+            animator.SetBool("isMovingDown", false);
+            animator.SetBool("isMovingLeft", false);
+            animator.SetBool("isMovingUp", false);
+
         }
         else if (Input.GetKey("w"))
         {
             chacacterController.GetComponent<Rigidbody2D>().MovePosition(chacacterController.GetComponent<Rigidbody2D>().position + speedY);
+            animator.SetBool("isMovingRight", false);
+            animator.SetBool("isMovingDown", false);
+            animator.SetBool("isMovingLeft", false);
+            animator.SetBool("isMovingUp", true);
         }
         else if (Input.GetKey("s"))
         {
             chacacterController.GetComponent<Rigidbody2D>().MovePosition(chacacterController.GetComponent<Rigidbody2D>().position - speedY);
+            animator.SetBool("isMovingRight", false);
+            animator.SetBool("isMovingDown", true);
+            animator.SetBool("isMovingLeft", false);
+            animator.SetBool("isMovingUp", false);
+        }
+        else
+        {
+            animator.SetBool("isMovingRight", false);
+            animator.SetBool("isMovingDown", false);
+            animator.SetBool("isMovingLeft", false);
+            animator.SetBool("isMovingUp", false);
+
         }
 
         // updates the stamina bar
