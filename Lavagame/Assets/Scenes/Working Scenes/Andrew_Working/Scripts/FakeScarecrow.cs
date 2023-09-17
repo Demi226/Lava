ca23456f358c53b246a6e134a4bf3c75ead86caa
FelixByte <bertrandrew@gmail.com>
2023-09-16 21:13:59 -0700

    fixed enemy controller, and got the fake scarecrow working

--- Lavagame/Assets/Scenes/Working Scenes/Andrew_Working/Scripts/FakeScarecrow.cs
+++ Lavagame/Assets/Scenes/Working Scenes/Andrew_Working/Scripts/FakeScarecrow.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class FakeScarecrow : MonoBehaviour
 {
-
+    public Transform scarecrowTrigger, playerTransform;
     public Transform scarecrowTransform;
     private float crowSpeed = 0f;
+    private float time = 100;
 
-    private void OnTriggerEnter2D()
-    {
-        crowSpeed = -0.005f;
-    }
 
     void Update()
     {
-        scarecrowTransform.Translate(crowSpeed * Time.deltaTime, 0f, 0f);
+        if( playerTransform.localPosition.x > scarecrowTrigger.localPosition.x)
+        {
+            crowSpeed = -0.05f;
+            time--;
+        }
+        if(time < 0){
+            Destroy(this.gameObject);
+        }
+
+        scarecrowTransform.Translate(crowSpeed, 0f, 0f);
     }
 }
